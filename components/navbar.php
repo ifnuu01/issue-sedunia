ea16083f9b235d0c928e8525e9119fb7b3c290b7
MatchaTi <tehmatcha28@gmail.com>
2024-10-28 18:49:56 +0800

    feat: add logout

--- components/navbar.php
+++ components/navbar.php
@@ -26,14 +26,15 @@ function navbar()
         </a>
     </div>
 
-    <a href="editProfile.php?id=' . htmlspecialchars($userID) . '" class="setting-icon setting w-12 h-12 flex items-center justify-center rounded-full">
-        <img src="assets/icons/setting.svg" alt="setting">
-    </a>
+    <div>
+        <a href="editProfile.php?id=' . htmlspecialchars($userID) . '" class="setting-icon setting w-12 h-12 flex items-center justify-center rounded-full">
+            <img src="assets/icons/setting.svg" alt="setting">
+        </a>
+        <a href="logout.php" class="setting-icon setting w-12 h-12 flex items-center justify-center rounded-full" onclick="return confirm(\'Yakin ingin logout?\')">
+            <img src="assets/icons/logout.svg" alt="logout">
+        </a>
+    </div>
 </nav>
         ';
 }
 ?>
-
-<nav>
-
-</nav>
